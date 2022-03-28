9a717a22389c9bbf9a4287b5254e4421dfaf623f
MisledWater079 <59891258+Misledwater79@users.noreply.github.com>
2022-03-28 03:34:25 -0400

    Made help command more like minecrafts

--- scripts/commands/help.js
+++ scripts/commands/help.js
@@ -4,9 +4,9 @@ import CommandHandler from "../classes/CommandRegistration.js"
 
 const registration = new CommandBuilder()
 .setName('help')
-.setAliases(['h'])
-.setDescription('get help on commands!')
-.setUsage(['help', 'help <command>'])
+.setAliases(['h','?','hlp'])
+.setDescription('Provides help/list of command')
+.setUsage(['<page: int>','[command: CommandName]'])
 .setCancelMessage(true)
 .setPrivate(false)
 .addInput(input => {
@@ -17,18 +17,100 @@ CommandHandler.register(registration, (interaction) => {
   try {
     const playerInput = interaction.command.getInput('command')?.getValue()
     
-    let message = `Command Prefix: ${CommandHandler.getPrefix()}\n`
+    let message = ``
     switch (!!playerInput) {
       case true:
+        if(Number(playerInput)) {
+          let pageRequest = 1
+          if(parseInt(playerInput) > 1){
+            pageRequest = pageRequest = parseInt(playerInput)
+          }
+          const commands = CommandHandler.getAllCommands()
+          let commandMap = []
+          for(const [key, command] of commands){
+            if(command.private) continue
+            for(const usage of command.usages){
+              commandMap.push([command.name, usage])
+            }
+            for(const aliase of command.aliases){
+              for(const usage of command.usages){
+                commandMap.push([aliase, usage])
+              }
+            }
+          }
+          commandMap.sort()
+          let pages = Math.round(commandMap.length/7.0+0.5)
+          if(pageRequest > pages){
+            message += `§2--- Showing help page ${pages} of ${pages} (/help <page>) ---\n§f`
+            for(let i = 0; i < 7; i++){
+              if(i + (pages-1) * 7 == commandMap.length) break;
+              message += `${CommandHandler.getPrefix()}${commandMap[i + (pages-1) * 7][0]} ${commandMap[i + (pages-1) * 7][1]}\n`
+            }
+            break;
+          }
+          message += `§2--- Showing help page ${pageRequest} of ${pages} (/help <page>) ---\n§f`
+          for(let i = 0; i < 7; i++){
+            if(i + (pageRequest-1) * 7 == commandMap.length) break;
+            message += `${CommandHandler.getPrefix()}${commandMap[i+(pageRequest-1)*7][0]} ${commandMap[i+(pageRequest-1)*7][1]}\n`
+          }
+          message += `§2Tip: You can use ${CommandHandler.getPrefix()}help <command: CommandName> to get more information about a certain command`
+          break;
+        }
         const command = CommandHandler.getCommand(playerInput)
-        message += !command || command?.private ? `§c${playerInput} was not found...` : `${command.name}:\n description: ${command.description}\n usage: ${command.usages ? command.usages : 'no usages were found' }\n aliases: ${command.aliases ? command.aliases : 'no aliases were found' }\n cooldown: ${command.cooldown ? command.cooldown : 'no cooldown was found' }`
+        if(!command || command?.private){
+          message += `§cUnknown command: ${playerInput}. Please check that the command exists and that you have permission to use it.`
+          break;
+        }
+        
+        if(command.aliases.length > 0){
+          for(const aliase of command.aliases){
+            if(aliase == command.aliases[0]){
+              message += `§e${command.name} (also ${aliase}`
+              continue
+            }
+            message += `, ${aliase}`
+          }
+          message += `)`
+        } else {
+          message += `§e${command.name}`
+        }
+        message += `:\n${command.description}.\n§fUsage:\n`
+        if(command.usages.length <= 1){
+          message += `- ${CommandHandler.getPrefix()}${command.name} ${command.usages ? command.usages : ' '}\n`
+        } else {
+          for(const usage of command.usages){
+            message += `- ${CommandHandler.getPrefix()}${command.name} ${usage}\n`
+          }
+        }
         break;
       case false:
         const commands = CommandHandler.getAllCommands()
-        for(const [key, command] of commands) {
+        let commandMap = []
+        for(const [key, command] of commands){
           if(command.private) continue
-          message += `${command.name}:\n description: ${command.description ? command.description : 'no description was found' }\n usage: ${command.usages ? command.usages : 'no usages were found' }\n aliases: ${command.aliases ? command.aliases : 'no aliases were found' }\n cooldown: ${command.cooldown ? command.cooldown : 'no cooldown was found' }\n\n`
+          for(const usage of command.usages){
+            commandMap.push([command.name, usage])
+          }
+          for(const aliase of command.aliases){
+            for(const usage of command.usages){
+              commandMap.push([aliase, usage])
+            }
+          }
         }
+        commandMap.sort()
+        if(commandMap.length < 7){
+          message += `§2--- Showing help page 1 of 1 (/help <page>) ---\n§f`
+          for(const command of commandMap){
+            message += `${CommandHandler.getPrefix()}${command[0]} ${command[1] ? command[1] : ' '}\n`
+          }
+          message += `§2Tip: You can use +help <command: CommandName> to get more information about a certain command`
+          break;
+        }
+        message += `§2--- Showing help page 1 of ${Math.round(commandMap.length/7.0+0.5)} (/help <page>) ---\n§f`
+        for(let i = 0; i < 7; i++){
+          message += `${CommandHandler.getPrefix()}${commandMap[i][0]} ${commandMap[i][1]}\n`
+        }
+        message += `§2Tip: You can use +help <command: CommandName> to get more information about a certain command`
         break;
       default:
         break;
